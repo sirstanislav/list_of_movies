526205868e854987acdff9fa5894e442ae52fa7b
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-02-20 12:49:30 +0100

    movies types

---
+++ src/interface/MoviesData.ts
@@ -0,0 +1,29 @@
+export type MoviesData = {
+  page?: string,
+  result: ResultOfIMoviesData[]
+  dates?: DatesOfIResultOfIMoviesData
+  total_pages?: number;
+  total_results?: number;
+}
+
+type ResultOfIMoviesData = {
+  poster_path?: string,
+  adult: boolean,
+  overview: string,
+  release_date: string,
+  genre_ids: number[];
+  id: number;
+  original_title: string,
+  original_language: string,
+  title: string,
+  backdrop_path?: string,
+  popularity: number,
+  vote_count: number,
+  video: boolean,
+  vote_average: number,
+}
+
+type DatesOfIResultOfIMoviesData = {
+  maximum: string,
+  minimum: string,
+}
