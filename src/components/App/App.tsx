96f93fe80285f2e3b8dfc39b3faf094c1926c774
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-02-20 12:48:22 +0100

    home and topRated routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,36 @@
 import "./App.scss";
 import { Main } from "../Main/Main";
+import { Header } from "../Header/Header";
+import { NotFoundPage } from "../../NotFoundPage/NotFoundPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const mainRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Main url={"/"}/>
+      </>
+    ),
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/upcoming",
+    element: (
+      <>
+        <Header />
+        <Main url={"/upcoming"}/>
+      </>
+    ),
+    errorElement: <NotFoundPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <RouterProvider router={mainRouter} />
     </div>
   );
 }
