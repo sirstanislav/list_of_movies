1ef0ae1665a8eff60257f0bd542a1cb68e2772e9
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-02-21 01:19:06 +0100

    difference component for different route

---
+++ src/components/MoviesCardList/TopRatedMoviesCardList.tsx
@@ -0,0 +1,92 @@
+import { useCallback, useEffect, useState } from "react";
+import { TopRatedMoviesCard } from "../MoviesCard/TopRatedMoviesCard";
+import { MoviesApi } from "../../api/MoviesApi";
+import { Container, Row } from "react-bootstrap";
+import { Preloader } from "../Preloader/Preloader";
+import { MovieData } from "../../interface/MovieData";
+
+interface ITopRatedMoviesCardList {}
+
+const TopRatedMoviesCardList: React.FC<ITopRatedMoviesCardList> = (props) => {
+  const [movies, setMovies] = useState<MovieData>({
+    page: "",
+    results: [],
+    dates: {
+      maximum: "",
+      minimum: "",
+    },
+    total_pages: 1,
+    total_results: 1,
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [counter, setCounter] = useState(1);
+
+  const loadMore = useCallback(() => {
+    setIsLoading(true);
+    setCounter((prevCounter) => {
+      MoviesApi.getTopRatedMovies(prevCounter)
+        .then((res) => {
+          setMovies((prevMovies) => {
+            const array = prevMovies.results.concat(res.results);
+            return { page: "", results: array };
+          });
+        })
+        .catch((err) => console.log(`Movie download error: ${err}`))
+        .finally(() => setIsLoading(false));
+
+      return prevCounter + 1;
+    });
+  }, []);
+
+  const windowScroll = useCallback(() => {
+    if (
+      window.innerHeight + window.pageYOffset >=
+      document.body.offsetHeight - 2
+    ) {
+      loadMore();
+    }
+  }, [loadMore]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", windowScroll, false);
+
+    return () => {
+      window.removeEventListener("scroll", windowScroll, false);
+    };
+  }, [windowScroll]);
+
+  useEffect(() => loadMore(), [loadMore]);
+
+  return isLoading ? (
+    <Container className="py-5">
+      <Row
+        xs={1}
+        md={2}
+        lg={4}
+        className="g-4"
+        style={{ paddingBottom: "300px" }}
+      >
+        {movies.results.map((movie: any, index: any) => {
+          return <TopRatedMoviesCard key={index} movie={movie} />;
+        })}
+      </Row>
+      <Preloader />
+    </Container>
+  ) : (
+    <Container className="py-5">
+      <Row
+        xs={1}
+        md={2}
+        lg={4}
+        className="g-4"
+        style={{ paddingBottom: "300px" }}
+      >
+        {movies.results.map((movie: any, index: any) => {
+          return <TopRatedMoviesCard key={index} movie={movie} />;
+        })}
+      </Row>
+    </Container>
+  );
+};
+
+export { TopRatedMoviesCardList };
