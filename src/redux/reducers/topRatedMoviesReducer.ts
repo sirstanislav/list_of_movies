76fc49ac843359ea9dc88202f6ef995f4e070cff
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-02-23 22:39:27 +0100

    replacing old way of creating slice

---
+++ src/redux/reducers/topRatedMoviesReducer.ts
@@ -0,0 +1,62 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { IMovieData } from '../../interface/IMovieData';
+import { all, put, takeLatest } from 'redux-saga/effects';
+import { MoviesApi } from '../../api/MoviesApi';
+import {
+  SET_TOP_RATED_MOVIES,
+  SET_TOP_RATED_MOVIES_ERROR
+} from "../constant"
+
+type TopRatedState = {
+  results: Array<{}>
+  error: string
+}
+
+const initialState: TopRatedState = {
+  results: [],
+  error: ''
+}
+
+const topRatedSlice = createSlice({
+  name: 'topRatedSlice',
+  initialState,
+  reducers: {
+    setStateTopRatedMovies(state = initialState, action: PayloadAction<IMovieData>) {
+      state.results = state.results.concat(action.payload.results)
+      return state
+    },
+    setError(state = initialState, action: PayloadAction<TopRatedState>) {
+      state.error = action.payload.error
+      return state
+    },
+  }
+})
+
+export const topRatedSliceActions = {
+  setTopRatedMovies: (payload: { counter: number }) => {
+    return {
+      type: SET_TOP_RATED_MOVIES,
+      payload
+    }
+  },
+  error: () => {
+
+  }
+}
+
+export function* getTopRatedMovies(action: { type: string, payload: { counter: number } }) {
+  try {
+    const data: IMovieData = yield MoviesApi.getTopRatedMovies(action.payload.counter);
+    yield put(topRatedSlice.actions.setStateTopRatedMovies(data))
+  } catch (error) {
+    yield put(topRatedSlice.actions.setError({ results: [], error: 'Error fetch' }))
+  }
+}
+
+export function* topRatedSaga() {
+  yield all([
+    takeLatest(SET_TOP_RATED_MOVIES, getTopRatedMovies)
+  ])
+}
+
+export default topRatedSlice
